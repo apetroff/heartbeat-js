9b53689cafb9587e278f1d6fa9ed3a53c94482e8
Alex Petrov <petroff.alex@gmail.com>
2012-05-30 19:01:42 +0400

    port changed

--- htdocs/health/health-monitor.js
+++ htdocs/health/health-monitor.js
@@ -1,7 +1,7 @@
 function updateStatus()
 {
     console.log(new Date().getTime());
-    jQuery('<img>').attr('id','statusImg').attr('src','http://health.local:50088/entity/health/update.json?url='+window.location.href+'&'+new Date().getTime()).remove();
+    jQuery('<img>').attr('id','statusImg').attr('src','http://health.local:50090/entity/health/update.json?url='+window.location.href+'&'+new Date().getTime()).remove();
 }
 function init()
 {
