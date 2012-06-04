c937081bda6a86be11b41d2276f52fd6571ef1f3
Alex Petrov <petroff.alex@gmail.com>
2012-06-04 12:30:12 +0400

    localhost used. cookie domain removed

--- htdocs/health/health-monitor.js
+++ htdocs/health/health-monitor.js
@@ -1,7 +1,7 @@
 function updateStatus()
 {
-    console.log(new Date().getTime());
-    jQuery('<img>').attr('id','statusImg').attr('src','http://health.local:50090/entity/health/update.json?url='+window.location.href+'&'+new Date().getTime()).remove();
+    //console.log(new Date().getTime());
+    jQuery('<img>').attr('id','statusImg').attr('src','http://localhost:50090/entity/health/update.json?url='+window.location.href+'&'+new Date().getTime()).remove();
 }
 function init()
 {
