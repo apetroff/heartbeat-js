114a1e262a1bad50491893805598293687338979
Alex Petrov <petroff.alex@gmail.com>
2012-06-13 20:10:01 +0400

    fix

--- htdocs/health/health-monitor.js
+++ htdocs/health/health-monitor.js
@@ -1,5 +1,4 @@
-(
- function(){
+(function(){
     var img = new Image();
     function updateStatus(){
         img.src = 'http://forumspb.dataflo.ws:50090/entity/health/update.json?url=' + document.URL + '&_=' + Date.now();
@@ -9,5 +8,4 @@
         updateStatus;
         ,5000
     );
-}()
-)
+}())
