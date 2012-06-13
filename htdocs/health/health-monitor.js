55d096f0b81af0c7645b69aec02bd0d16e634862
Alex Petrov <petroff.alex@gmail.com>
2012-06-13 15:02:55 +0400

    url changed to forumspb.dataflo.ws

--- htdocs/health/health-monitor.js
+++ htdocs/health/health-monitor.js
@@ -1,7 +1,7 @@
 function updateStatus()
 {
     //console.log(new Date().getTime());
-    jQuery('<img>').attr('id','statusImg').attr('src','http://localhost:50090/entity/health/update.json?url='+window.location.href+'&'+new Date().getTime()).remove();
+    jQuery('<img>').attr('id','statusImg').attr('src','http://forumspb.dataflo.ws:50090/entity/health/update.json?url='+window.location.href+'&'+new Date().getTime()).remove();
 }
 function init()
 {
